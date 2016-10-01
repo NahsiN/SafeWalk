e20af0451fd42cd789b5afa3df06ec1132b9dce4
Nishan Mann <nishan.singh.mann@gmail.com>
2016-10-01 19:53:11 -0400

    Configure to run using uWSGI and nginx

--- crime_app/__init__.py
+++ crime_app/__init__.py
@@ -3,6 +3,9 @@ import sys
 
 app = Flask(__name__)
 # path where to save routes.html
-app.path = sys.argv[1]
+#app.path = sys.argv[1]
+#----------------------#
+app.path='/home/ubuntu/'
+#---------------------#
 # print(app.path)
 from crime_app import views
